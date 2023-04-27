221f708e70fdffe23be61badbe1b3adc916239d2
mayer malpud <129673098+1201104027@users.noreply.github.com>
2023-04-27 10:59:33 -0500

    implementacion de los activity's sin diseño

---
+++ app/src/main/java/com/example/myart/MultimediaActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MultimediaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_multimedia)
+    }
+}
