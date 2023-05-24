9cb77737b1c8ea20a92822409c151bd059de5c4b
mayer malpud <129673098+1201104027@users.noreply.github.com>
2023-05-24 11:47:23 -0500

    diseño de la interfaz de camara

--- app/src/main/java/com/example/myart/CameraActivity.kt
+++ app/src/main/java/com/example/myart/CameraActivity.kt
@@ -13,10 +13,13 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat.startActivityForResult
+import java.io.IOException
 
 class CameraActivity : AppCompatActivity() {
 
-    lateinit var _con_con: ImageView
+
+    lateinit var con_con: ImageView
     lateinit var _tip_con: Button
     lateinit var _txt_con: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,36 +28,49 @@ class CameraActivity : AppCompatActivity() {
 
         val btnGallery = findViewById<ImageView>(R.id.btn_gallery)
         val btnCamera = findViewById<ImageView>(R.id.btn_camera)
+        con_con = findViewById(R.id.iv_content)
 
-
-        btnCamera.setOnClickListener{
+        btnCamera.setOnClickListener {
             Toast.makeText(this, "camera open", Toast.LENGTH_LONG).show()
             startForResultRight.launch(Intent(MediaStore.ACTION_IMAGE_CAPTURE))
         }
+
+        //btnUpload.setOnClickListener { }
+
         btnGallery.setOnClickListener {
-            val intent = Intent(Intent.ACTION_PICK)
-            intent.type = "image/*"
-            startActivityForResult(intent, 1)
+            showFileChooser()
         }
+
+
     }
+    private val PICK_IMAGE_REQUEST = 1
+    private fun showFileChooser() {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(Intent.createChooser(intent, "Selecciona imagen"), PICK_IMAGE_REQUEST)
+    }
+
+    private val startForResultRight =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: androidx.activity.result.ActivityResult ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val intent = result.data
+                var imageBitmap = intent?.extras?.get("data") as Bitmap
+                con_con.setImageBitmap(imageBitmap)
+            }
+        }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
-            val imageUri: Uri? = data?.data
-            val image = findViewById<ImageView>(R.id.iv_content)
-            image.setImageURI(imageUri)
-        }
-
-    }
-
-    private val startForResultRight = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-            result: androidx.activity.result.ActivityResult ->
-        if (result.resultCode == Activity.RESULT_OK){
-            val intent = result.data
-            val imageBitmap = intent?.extras?.get("data") as Bitmap
-            val image = findViewById<ImageView>(R.id.iv_content)
-            image.setImageBitmap(imageBitmap)
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null && data.data != null) {
+            val filePath: Uri = data.data!!
+            try {
+                var bitmap = MediaStore.Images.Media.getBitmap(contentResolver, filePath)
+                con_con.setImageBitmap(bitmap)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
         }
     }
 }
