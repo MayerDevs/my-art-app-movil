b9d637099afbf77906350f8853dd8a29be64ab62
mayer malpud <129673098+1201104027@users.noreply.github.com>
2023-05-25 12:40:28 -0500

    recycler completo

--- app/src/main/java/com/example/myart/MainActivity.kt
+++ app/src/main/java/com/example/myart/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Website.URL
 import android.util.Log
 import android.widget.ImageView
 import android.widget.Toast
@@ -14,6 +15,9 @@ import com.example.myart.clases.adapters.ContentAdapter
 import com.example.myart.data.Content
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import java.io.IOException
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,6 +28,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var user: ImageView
     lateinit var upload_resource: ImageView
     private val auth = FirebaseAuth.getInstance()
+    lateinit var camera: ImageView
 
     lateinit var search: ImageView
 
@@ -40,7 +45,7 @@ class MainActivity : AppCompatActivity() {
         image = findViewById(R.id.iv_image)
         music = findViewById(R.id.iv_music)
         user = findViewById(R.id.iv_user)
-        upload_resource = findViewById(R.id.iv_upload_resource)
+        camera = findViewById(R.id.iv_camera)
         search = findViewById(R.id.iv_search)
 
 
@@ -58,17 +63,46 @@ class MainActivity : AppCompatActivity() {
                 val imageList = mutableListOf<String>()
 
                 // Iterar sobre los documentos
-                for (document in documents) {
-                    // Obtener el campo "con_con" de cada documento
-                    val imageUrl = document.getString("con_con")
+                Thread {
+                    // Iterar sobre los documentos
+                    for (document in documents) {
+                        // Obtener el campo "con_con" de cada documento
+                        val imageUrl = document.getString("con_con")
 
-                    // Agregar la URL de la imagen a la lista
-                    imageUrl?.let {
-                        (imageList as MutableList<String>).add(it)
+                        // Verificar si la imagen existe
+                        imageUrl?.let {
+                            try {
+                                val url = URL(it)
+                                val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+                                connection.requestMethod = "HEAD"
+
+                                // Obtener el código de respuesta
+                                val responseCode = connection.responseCode
+
+                                if (responseCode == HttpURLConnection.HTTP_OK) {
+                                    // La imagen existe, agregar la URL a la lista
+                                    runOnUiThread {
+                                        imageList.add(it)
+                                    }
+                                } else {
+                                    // La imagen no existe, omitirla
+                                    Log.e(TAG, "La imagen no existe: $it")
+                                }
+
+                                connection.disconnect()
+                            } catch (e: IOException) {
+                                // Error al verificar la existencia de la imagen
+                                Log.e(TAG, "Error al verificar la existencia de la imagen: $it", e)
+                            }
+                        }
                     }
-                }
-                contentAdapter = ContentAdapter(this, imageList)
-                recyclerView.adapter = contentAdapter
+
+                    // Actualizar el RecyclerView en el hilo principal
+                    runOnUiThread {
+                        contentAdapter = ContentAdapter(this, imageList)
+                        recyclerView.adapter = contentAdapter
+                    }
+                }.start()
             }
             .addOnFailureListener { exception ->
                 // El método onFailure se ejecuta si ocurre algún error durante la consulta
@@ -108,7 +142,7 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        upload_resource.setOnClickListener{
+        camera.setOnClickListener{
             Toast.makeText(this, "camera.", Toast.LENGTH_SHORT).show()
             val i = Intent(this, CameraActivity::class.java)
             startActivity(i)
@@ -116,7 +150,7 @@ class MainActivity : AppCompatActivity() {
 
         search.setOnClickListener{
             Toast.makeText(this, "search.", Toast.LENGTH_SHORT).show()
-            val i = Intent(this, SearchActivity::class.java)
+            val i = Intent(this, CameraActivity::class.java)
             startActivity(i)
         }
     }
